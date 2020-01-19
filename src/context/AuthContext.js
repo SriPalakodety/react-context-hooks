481f4dbe22a0e92d6d03eebbfac35cafd45b6723
Sri Palakodety <saisrinivas.palakodety@nike.com>
2020-01-18 17:09:26 -0800

    Refator context files to use hooks

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,22 +1,17 @@
-import React, { Component, createContext } from 'react';
+import React, { useState, createContext } from 'react';
 
 export const AuthContext = createContext();
 
-class AuthContextProvider extends Component {
-	state = {
-		isAuthenticated: false,
+function AuthContextProvider(props) {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const toggleAuth = () => {
+		setIsAuthenticated(!isAuthenticated);
 	};
-	toggleAuth = () => {
-		this.setState({ isAuthenticated: !this.state.isAuthenticated });
-	};
-	render() {
-		const { children } = this.props;
-		return (
-			<AuthContext.Provider value={{ ...this.state, toggleAuth: this.toggleAuth }}>
-				{children}
-			</AuthContext.Provider>
-		);
-	}
+	return (
+		<AuthContext.Provider value={{ isAuthenticated, toggleAuth: toggleAuth }}>
+			{props.children}
+		</AuthContext.Provider>
+	);
 }
 
 export default AuthContextProvider;
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,33 +1,28 @@
-import React, { Component, createContext } from 'react';
+import React, { useState, createContext } from 'react';
 
 export const ThemeContext = createContext();
 
-class ThemeContextProvider extends Component {
-	state = {
-		isLightTheme: true,
-		light: {
-			color: '#555',
-			ui: '#ddd',
-			bg: '#eee',
-		},
-		dark: {
-			color: '#ddd',
-			ui: '#333',
-			bg: '#555',
-		},
-	};
+function ThemeContextProvider(props) {
+	const [isLightTheme, setIsLightTheme] = useState(true);
+	const [light] = useState({
+		color: '#555',
+		ui: '#ddd',
+		bg: '#eee',
+	});
+	const [dark] = useState({
+		color: '#ddd',
+		ui: '#333',
+		bg: '#555',
+	});
 
-	toggleTheme = () => {
-		this.setState({ isLightTheme: !this.state.isLightTheme });
+	const toggleTheme = () => {
+		setIsLightTheme(!isLightTheme);
 	};
-	render() {
-		const { children } = this.props;
-		return (
-			<ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
-				{children}
-			</ThemeContext.Provider>
-		);
-	}
+	return (
+		<ThemeContext.Provider value={{ isLightTheme, light, dark, toggleTheme: toggleTheme }}>
+			{props.children}
+		</ThemeContext.Provider>
+	);
 }
 
 export default ThemeContextProvider;
